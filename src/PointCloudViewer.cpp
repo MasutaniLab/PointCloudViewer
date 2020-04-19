43e6831e85a6f11b330598c0a763f0552e46f208
y-masutani <masutani@isc.osakac.ac.jp>
2020-04-20 07:24:27 +0900

    confの設定を標準に戻す．例外処理を追加（vector too longでエラー状態に遷移しないようにした）．

--- src/PointCloudViewer.cpp
+++ src/PointCloudViewer.cpp
@@ -184,106 +184,124 @@ RTC::ReturnCode_t PointCloudViewer::onDeactivated(RTC::UniqueId ec_id)
 
 RTC::ReturnCode_t PointCloudViewer::onExecute(RTC::UniqueId ec_id)
 {
-  if (m_pcIn.isNew())
-  {
-    m_pcIn.read();
-    if (m_first) {
-      RTC_INFO_VAR(m_pc.seq);
-      RTC_INFO_VAR(m_pc.height);
-      RTC_INFO_VAR(m_pc.width);
-      RTC_INFO_VAR(m_pc.type);
-      RTC_INFO_VAR(m_pc.is_bigendian);
-      RTC_INFO_VAR(m_pc.point_step);
-      RTC_INFO_VAR(m_pc.row_step);
-      RTC_INFO_VAR(m_pc.is_dense);
-      for (size_t i=0; i<m_pc.fields.length(); i++) {
-        RTC_INFO_POINT_FIELD(i, m_pc.fields[i]);
+  try {
+    if (m_pcIn.isNew()) {
+      m_pcIn.read();
+      if (m_first) {
+        RTC_INFO_VAR(m_pc.seq);
+        RTC_INFO_VAR(m_pc.height);
+        RTC_INFO_VAR(m_pc.width);
+        RTC_INFO_VAR(m_pc.type);
+        RTC_INFO_VAR(m_pc.is_bigendian);
+        RTC_INFO_VAR(m_pc.point_step);
+        RTC_INFO_VAR(m_pc.row_step);
+        RTC_INFO_VAR(m_pc.is_dense);
+        for (size_t i = 0; i < m_pc.fields.length(); i++) {
+          RTC_INFO_POINT_FIELD(i, m_pc.fields[i]);
+        }
+        m_first = false;
       }
-      m_first = false;
-    }
-    string type = string(m_pc.type);
-    if (type == "xyz") {
-      pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
-      pcl_cloud->is_dense = m_pc.is_dense;
-      pcl_cloud->points.resize(m_pc.width*m_pc.height);
-      float *src = (float *)m_pc.data.get_buffer();
-      for (size_t i=0; i<pcl_cloud->points.size(); i++){
-        pcl_cloud->points[i].x = src[0];
-        pcl_cloud->points[i].y = src[1];
-        pcl_cloud->points[i].z = src[2];
-        src += 4;
-      }
-      if (!m_viewer->updatePointCloud(pcl_cloud, "cloud")) {
-        m_viewer->addPointCloud<pcl::PointXYZ>(pcl_cloud, "cloud");
-        m_viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
-      }
-    } else if (type == "xyzrgb") {
-      pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
-      pcl_cloud->points.resize(m_pc.width*m_pc.height);
-      pcl_cloud->is_dense = m_pc.is_dense;
-      float *src = (float *)m_pc.data.get_buffer();
-      for (size_t i=0; i<pcl_cloud->points.size(); i++){
-        pcl_cloud->points[i].x = src[0];
-        pcl_cloud->points[i].y = src[1];
-        pcl_cloud->points[i].z = src[2];
-        pcl_cloud->points[i].rgb = src[3];
+      string type = string(m_pc.type);
+      if (type == "xyz") {
+        pcl::PointCloud<pcl::PointXYZ>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+        pcl_cloud->is_dense = m_pc.is_dense;
+        pcl_cloud->points.resize(m_pc.width * m_pc.height);
+        float* src = (float*)m_pc.data.get_buffer();
+        for (size_t i = 0; i < pcl_cloud->points.size(); i++) {
+          pcl_cloud->points[i].x = src[0];
+          pcl_cloud->points[i].y = src[1];
+          pcl_cloud->points[i].z = src[2];
+          src += 4;
+        }
+        if (!m_viewer->updatePointCloud(pcl_cloud, "cloud")) {
+          m_viewer->addPointCloud<pcl::PointXYZ>(pcl_cloud, "cloud");
+          m_viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
+        }
+      } else if (type == "xyzrgb") {
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+        pcl_cloud->points.resize(m_pc.width * m_pc.height);
+        pcl_cloud->is_dense = m_pc.is_dense;
+        float* src = (float*)m_pc.data.get_buffer();
+        for (size_t i = 0; i < pcl_cloud->points.size(); i++) {
+          pcl_cloud->points[i].x = src[0];
+          pcl_cloud->points[i].y = src[1];
+          pcl_cloud->points[i].z = src[2];
+          pcl_cloud->points[i].rgb = src[3];
 #ifdef SWAP_R_B      
-        uint8_t t = pcl_cloud->points[i].r;
-        pcl_cloud->points[i].r = pcl_cloud->points[i].b;
-        pcl_cloud->points[i].b = t;
+          uint8_t t = pcl_cloud->points[i].r;
+          pcl_cloud->points[i].r = pcl_cloud->points[i].b;
+          pcl_cloud->points[i].b = t;
 #endif
 #if 0
-        int x = i%m_pc.width;
-        int y = i/m_pc.width; 
-        cout << i << "  " << x << "," << y << "  "
-          << pcl_cloud->points[i].x << "," << pcl_cloud->points[i].y << "," << pcl_cloud->points[i].z << "  "
-          <<  setfill('0') << setw(8) << right << hex << *(uint32_t *)&pcl_cloud->points[i].rgb << dec << endl;
-#endif
-#if 0
-        int x = i%m_pc.width;
-        int y = i/m_pc.width;
-        if ( x == m_pc.width/2) {
+          int x = i % m_pc.width;
+          int y = i / m_pc.width;
           cout << i << "  " << x << "," << y << "  "
             << pcl_cloud->points[i].x << "," << pcl_cloud->points[i].y << "," << pcl_cloud->points[i].z << "  "
-            <<  setfill('0') << setw(8) << right << hex << *(uint32_t *)&pcl_cloud->points[i].rgb << dec << endl;
+            << setfill('0') << setw(8) << right << hex << *(uint32_t*)&pcl_cloud->points[i].rgb << dec << endl;
+#endif
+#if 0
+          int x = i % m_pc.width;
+          int y = i / m_pc.width;
+          if (x == m_pc.width / 2) {
+            cout << i << "  " << x << "," << y << "  "
+              << pcl_cloud->points[i].x << "," << pcl_cloud->points[i].y << "," << pcl_cloud->points[i].z << "  "
+              << setfill('0') << setw(8) << right << hex << *(uint32_t*)&pcl_cloud->points[i].rgb << dec << endl;
+          }
+#endif
+          src += 4;
         }
-#endif
-        src += 4;
-      }
-      if (!m_viewer->updatePointCloud(pcl_cloud, "cloud")) {
-        m_viewer->addPointCloud<pcl::PointXYZRGB>(pcl_cloud, "cloud");
-        m_viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
-      }
+        if (!m_viewer->updatePointCloud(pcl_cloud, "cloud")) {
+          m_viewer->addPointCloud<pcl::PointXYZRGB>(pcl_cloud, "cloud");
+          m_viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
+        }
     } else if (type == "xyzrgba") {
-      pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
-      pcl_cloud->points.resize(m_pc.width*m_pc.height);
-      pcl_cloud->is_dense = m_pc.is_dense;
-      float *src = (float *)m_pc.data.get_buffer();
-      for (size_t i=0; i<pcl_cloud->points.size(); i++){
-        pcl_cloud->points[i].x = src[0];
-        pcl_cloud->points[i].y = src[1];
-        pcl_cloud->points[i].z = src[2];
-        pcl_cloud->points[i].rgb = src[3];
+        pcl::PointCloud<pcl::PointXYZRGBA>::Ptr pcl_cloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
+        pcl_cloud->points.resize(m_pc.width * m_pc.height);
+        pcl_cloud->is_dense = m_pc.is_dense;
+        float* src = (float*)m_pc.data.get_buffer();
+        for (size_t i = 0; i < pcl_cloud->points.size(); i++) {
+          pcl_cloud->points[i].x = src[0];
+          pcl_cloud->points[i].y = src[1];
+          pcl_cloud->points[i].z = src[2];
+          pcl_cloud->points[i].rgb = src[3];
 #ifdef SWAP_R_B      
-        uint8_t t = pcl_cloud->points[i].r;
-        pcl_cloud->points[i].r = pcl_cloud->points[i].b;
-        pcl_cloud->points[i].b = t;
+          uint8_t t = pcl_cloud->points[i].r;
+          pcl_cloud->points[i].r = pcl_cloud->points[i].b;
+          pcl_cloud->points[i].b = t;
 #endif
-        src += 4;
+          src += 4;
+        }
+        if (!m_viewer->updatePointCloud(pcl_cloud, "cloud")) {
+          m_viewer->addPointCloud<pcl::PointXYZRGBA>(pcl_cloud, "cloud");
+          m_viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
+        }
+      } else {
+        RTC_ERROR(((type + ": not supported").c_str()));
+        return RTC::RTC_ERROR;
       }
-      if (!m_viewer->updatePointCloud(pcl_cloud, "cloud")) {
-        m_viewer->addPointCloud<pcl::PointXYZRGBA>(pcl_cloud, "cloud");
-        m_viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "cloud");
-      }
-    } else {
-      RTC_ERROR(((type + ": not supported").c_str()));
-      return RTC::RTC_ERROR;
     }
+    m_viewer->spinOnce(10);
+    if (m_viewer->wasStopped()) {
+        // Deactivate self
+    }
+  } catch (const std::length_error& e) {
+    string w = e.what();
+    if (w == "vector too long") {
+      //pcl::visualization::PCLVisualizerInteractorStyle::OnTimer () 内の
+      //Interactor->Render ()で発生する例外を回避するための苦肉の策
+      RTC_WARN(("例外発生: %s (無視)", e.what()));
+      return RTC::RTC_OK;
+    }
+    RTC_ERROR(("例外発生: %s", e.what()));
+    return RTC::RTC_ERROR;
+  } catch (const std::exception& e) {
+    RTC_ERROR(("例外発生: %s", e.what()));
+    return RTC::RTC_ERROR;
+  } catch (...) {
+    RTC_ERROR(("onExecute()において非標準の例外発生"));
+    return RTC::RTC_ERROR;
   }
-  m_viewer->spinOnce();
-  if (m_viewer->wasStopped()) {
-      // Deactivate self
-  }
+
   return RTC::RTC_OK;
 }
 
